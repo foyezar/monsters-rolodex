bbf2bc3ab4f1cfb3fce005503cc213515d065dfd
foyez <foyezar@yahoo.com>
2020-01-16 00:25:15 +0600

    Updates

---
+++ precache-manifest.e399ab64307b497fff7d88d8a2cd7bf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8f687c652d5344594cf9d26b1d56c2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1278a985be75993b0507",
+    "url": "/monsters-rolodex/static/css/main.30c5c05e.chunk.css"
+  },
+  {
+    "revision": "ef607de0cbefad24596f",
+    "url": "/monsters-rolodex/static/js/2.ab85827f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.ab85827f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1278a985be75993b0507",
+    "url": "/monsters-rolodex/static/js/main.21ce4a6d.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
